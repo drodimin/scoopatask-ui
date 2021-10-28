2c1c0899eb311c10838b7a5cede090d04be21b55
drodimin <49958755+drodimin@users.noreply.github.com>
2021-10-27 20:00:31 -0400

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Shell from './components/Shell';
+import { Typography } from '@mui/material';
 
 
 const App = () => {  
-  return (
-    <Router>
-      <Route exact path="/" component={Shell} />
-    </Router>
-  );
+  if(process.env.REACT_APP_BLOCK && process.env.REACT_APP_BLOCK === 'Yes') {
+    return <Typography sx={{m:"auto"}} width="fit-content" variant="h4">Service Not Available</Typography>
+  }
+  else {
+    return <Router>
+          <Route exact path="/" component={Shell} />
+        </Router>
+  }
 }
 
 export default App;
